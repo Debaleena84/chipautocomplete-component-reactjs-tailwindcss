1e5ff3ab9e8a59f9be49a2d8c76ea3724c9f95fa
Debaleena84 <85742267+Debaleena84@users.noreply.github.com>
2024-06-06 11:10:38 +0530

    Initialized and finalized App component with ChipAutoComplete and CSS styles.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChipAutoComplete from './components/ChipAutoComplete';
+import './styles.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <ChipAutoComplete />
+        </div>
+    );
+}
+
+export default App;
